7f44a9b8f5055a0adc4404b27a596e7762688a58
Jacob Lindahl <aviator9402@gmail.com>
2016-10-11 18:59:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Image Carousel/Slider
+
+A simple image carousel/slider in JavaScript and Sass.
+
+[Video Guide](https://youtu.be/QGcWZUjGDGs)
---
+++ image-carousel.js
@@ -0,0 +1,58 @@
+let carousels = document.getElementsByClassName('image-carousel');
+
+[].forEach.call(carousels, function (c) {
+    let next = c.getElementsByClassName('next')[0],
+        prev = c.getElementsByClassName('prev')[0],
+        bubblesContainer = c.getElementsByClassName('bubbles')[0],
+        inner = c.getElementsByClassName('inner')[0],
+        imgs = inner.getElementsByTagName('img'),
+        currentImageIndex = 0,
+        width = 640,
+        bubbles = [];
+
+    for (let i = 0; i < imgs.length; i++) {
+        let b = document.createElement('span');
+        b.classList.add('bubble');
+        bubblesContainer.appendChild(b);
+        bubbles.push(b);
+
+        b.addEventListener('click', function () {
+            currentImageIndex = i;
+            switchImg();
+        });
+    }
+
+    function switchImg () {
+        inner.style.left = -width * currentImageIndex + 'px';
+        
+        bubbles.forEach(function (b, i) {
+            if (i === currentImageIndex) {
+                b.classList.add('active');
+            } else {
+                b.classList.remove('active');
+            }
+        });
+    }
+
+    next.addEventListener('click', function () {
+        currentImageIndex++;
+
+        if (currentImageIndex >= imgs.length) {
+            currentImageIndex = 0;
+        }
+
+        switchImg();
+    });
+
+    prev.addEventListener('click', function () {
+        currentImageIndex--;
+
+        if (currentImageIndex < 0) {
+            currentImageIndex = imgs.length - 1;
+        }
+
+        switchImg();
+    });
+
+    switchImg();
+});
